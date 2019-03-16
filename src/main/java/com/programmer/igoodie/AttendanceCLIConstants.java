a70d1ce57ca4b1fb14d9494c59a930278519d247
Anılcan <igoodie@programmer.net>
2019-03-17 01:50:48 +0300

    chore: Change obscure version token

--- src/main/java/com/programmer/igoodie/AttendanceCLIConstants.java
+++ src/main/java/com/programmer/igoodie/AttendanceCLIConstants.java
@@ -3,6 +3,6 @@ package com.programmer.igoodie;
 public interface AttendanceCLIConstants {
 
 	String PROGRAM_NAME = "Attendance-CLI";
-	String PROGRAM_VERSION = "0.1.0a";
+	String PROGRAM_VERSION = "0.1.0-ALPHA";
 	
 }
